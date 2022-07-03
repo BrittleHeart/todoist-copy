7507f4fc866cd187401c5db07408325ff6207a4f
Bartosz <b.pazdur@yahoo.com>
2022-07-03 09:17:45 +0200

    Bug fixing and improvements
    Delete useless interface and fix kernel issues

--- server/app/IApplication.ts
+++
@@ -1,6 +0,0 @@
-export interface IApplication {
-  _port: number,
-  _host: string,
-  /* eslint-disable */
-  bootstrap: (port: number, host?: string) => Promise<void>
-}
--- server/app/Kernel.ts
+++ server/app/Kernel.ts
@@ -1,14 +1,22 @@
-import { IApplication } from './IApplication'
-export default class Kernel implements IApplication {
-  readonly _port: number
-  readonly _host: string
+import { InversifyExpressServer } from 'inversify-express-utils'
+import { Container, inject, injectable } from 'inversify'
+import { Application } from 'express'
+import GlobalMiddleware from './http/middlewares/GlobalMiddleware'
 
-  constructor(private readonly port?: number, private readonly host?: string) {
-    this._port = port ? port : 3000
-    this._host = host ? host : 'localhost'
+export const container: Container = new Container
+@injectable()
+export default class Kernel {
+  public readonly app: InversifyExpressServer | Application
+
+  constructor(@inject(GlobalMiddleware) protected readonly globalMiddlewares: GlobalMiddleware) {
+    this.app = new InversifyExpressServer(container)
+    this.globalMiddlewares = new GlobalMiddleware()
+    this.app = this.app.build()
+    globalMiddlewares.handle(this.app)
   }
-
-  public async bootstrap() {
-    console.log('hehe')
+  
+  public async run() {
+    // @ts-ignore
+    this.app.listen(3000, () => console.log('Server is listening on http://localhost:3000'))
   }
 }
